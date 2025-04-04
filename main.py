b55f37da1b0af80e3dc77b3ce44348cbadfa474a
TVA018 <thomas.vu018@gmail.com>
2025-04-03 21:24:57 -0400

    Complete

--- main.py
+++ main.py
@@ -1,35 +1,50 @@
 import requests
 import sys
 
-api_key = "0DowkZk6qFmZnOJNxCbySSjS7qHLOJE6rJwxERfY1pwS25bifGjZa9x7Ax5EVlqc"
+print("Enter your API Key: ", end="")
+api_key = input()
 url = "https://www.thebluealliance.com/api/v3"
 headers = {
     "X-TBA-Auth-Key": api_key
 }
-event_key = "2025oncmp1"
-
-teamsJson = requests.get(f"{url}/event/{event_key}/teams", headers=headers).json()
-teamsJson.sort(key=lambda team: team["team_number"])
+eventKey = "2025oncmp1"
 
 progress = 0
 
-def generateText():
+def getData():
     global progress
+
+    teamsJson = requests.get(f"{url}/event/{eventKey}/teams/simple", headers=headers).json()
+    teamsJson.sort(key=lambda team: team["team_number"])
+
     finalText = ""
     numTeams = len(teamsJson)
 
     for team in teamsJson:
-        teamMatches = requests.get(f"{url}/team/{team["key"]}/event/{event_key}/matches", headers=headers).json()
+        teamKey = team["key"]
+        teamNumber = team["team_number"]
+        teamName = team["nickname"]
+
+        teamMatches = requests.get(f"{url}/team/{teamKey}/event/{eventKey}/matches/simple", headers=headers).json()
         teamMatches.sort(key=lambda match: match["match_number"])
 
-        finalText += f"# {team["team_number"]}: {team["nickname"]}\n"
+        finalText += f"{teamNumber}: {teamName}\n"
 
         for match in teamMatches:
-            finalText += f"- Q{match["match_number"]}\n"
+            alliance = ""
+            position = 0
+
+            for allianceColor, allianceTeam in match["alliances"].items():
+                if(teamKey in allianceTeam["team_keys"]):
+                    position = allianceTeam["team_keys"].index(teamKey) + 1
+                    alliance = allianceColor.title()
+                    break
+
+            finalText += f"- Q{match["match_number"]} ({alliance} {position})\n"
 
         progress += 1
         print(f"{round(progress/numTeams * 100)}%", end="\r")
 
     print(finalText)
 
-generateText()
+getData()
